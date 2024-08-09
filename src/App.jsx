c1cc813bd6806e01735e2a86e50b6b6ee30a9868
KRISHNA BHISE <bhisekrishnaa@gmail.com>
2024-08-09 21:33:17 +0530

    created pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,22 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import {Router,Routes,BrowserRouter, Route} from 'react-router-dom'
+import SignIn from './Pages/SignIn'
+import SignUp from './Pages/SignUp'
+import Home from './Pages/Home'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
- <h1>App
-    </h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<SignIn/>}/>
+        <Route path='/sign-up' element={<SignUp/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
