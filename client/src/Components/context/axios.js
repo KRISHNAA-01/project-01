c8a990b163f61d07d2bdd839fdd3aca065b6b0dd
KRISHNA BHISE <bhisekrishnaa@gmail.com>
2025-01-18 15:54:38 +0530

    api connect for deployment

--- client/src/Components/context/axios.js
+++ client/src/Components/context/axios.js
@@ -1,9 +1,13 @@
 import axios from 'axios';
 
 // Set up a default Axios instance with `withCredentials: true`
+// const api = axios.create({
+//     baseURL: '/api', // Update to your actual backend URL
+//     withCredentials: true, // This ensures cookies are sent in each request
+// });
+// After: Update for production
 const api = axios.create({
-    baseURL: '/api', // Update to your actual backend URL
-    withCredentials: true, // This ensures cookies are sent in each request
+    baseURL: 'https://project-01-1-vhio.onrender.com/api', // Replace with your deployed backend URL
+    withCredentials:true,
 });
-
 export default api;
